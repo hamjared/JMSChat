c2d886cab1749fe24e090eb5dd463868fbbb72e9
Jared <hamjared5@gmail.com>
2020-08-24 13:14:37 -0600

    push

---
+++ JMSChat/src/Chat.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+public class Chat implements Runnable {
+
+	private List<String> chat;
+	private static ConnectionFactory connectionFactory = null;
+	private static Topic defaultTopic = null;
+
+	public Chat() {
+		chat = new ArrayList<>();
+		connectionFactory = CommonSettings.getConnectionFactory();
+		defaultTopic = CommonSettings.getDefautTopic();
+	}
+
+	public void sendMessage(String msg) {
+		System.out.println("Sending Message");
+		JMSContext jmsContext = connectionFactory.createContext();
+		JMSProducer producer = jmsContext.createProducer();
+		producer.send(defaultTopic, msg);
+		System.out.println("Message sent");
+
+	}
+
+	public void receiveMessage(String msg) {
+
+		if (msg == null) {
+			System.out.println("String is null");
+			return;
+		}
+		chat.add(msg);
+		System.out.println(msg);
+	}
+
+
+	@Override
+	public void run() {
+		JMSContext jmsContext = connectionFactory.createContext();
+		jmsContext.setClientID("exampleApp");
+		JMSConsumer consumer = jmsContext.createDurableConsumer(defaultTopic, "Test");
+		Message msg;
+
+		while (true) {
+
+			msg =  consumer.receive(500);
+			try {
+				if (msg != null) {
+					receiveMessage(msg.getBody(String.class));
+
+				}
+			} catch (JMSException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		Chat chat = new Chat();
+		chat.sendMessage("Hello");
+		Thread thread = new Thread(chat);
+		thread.start();
+		thread.join();
+
+	}
+
+}
---
+++ JMSChat/src/CommonSettings.java
@@ -0,0 +1,46 @@
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class CommonSettings {
+  private static ConnectionFactory CONNECTION_FACTORY = null;
+  private static Queue PTP_QUEUE = null;
+  private static Topic PUB_SUB_TOPIC = null;
+  private static Queue DEFAULT_REPLY_QUEUE = null;
+  private static Topic DEFAULT_MESSAGE_TOPIC = null;
+
+  static {
+    try {
+      InitialContext initialContext = new InitialContext();
+      CONNECTION_FACTORY = (ConnectionFactory) initialContext.lookup("jms/__defaultConnectionFactory");
+      PTP_QUEUE = (Queue) initialContext.lookup("jms/PTPQueue");
+      DEFAULT_REPLY_QUEUE = (Queue) initialContext.lookup("jms/ReplyQueue");
+      PUB_SUB_TOPIC = (Topic) initialContext.lookup("jms/PubSubTopic");
+      DEFAULT_MESSAGE_TOPIC = (Topic) initialContext.lookup("jms/MessageQueue");
+    } catch (NamingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static ConnectionFactory getConnectionFactory() {
+    return CONNECTION_FACTORY;
+  }
+
+  public static Queue getDefaultQueue() {
+    return PTP_QUEUE;
+  }
+
+  public static Queue getDefaultReplyQueue() {
+    return DEFAULT_REPLY_QUEUE;
+  }
+
+  public static Topic getDefautTopic() {
+    return PUB_SUB_TOPIC;
+  }
+  
+  public static Topic getMessageTopic() {
+	    return DEFAULT_MESSAGE_TOPIC;
+	  }
+}
