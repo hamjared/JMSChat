f26f9494f90c9b873d11288ebb02ce563d3defd5
Jared <hamjared5@gmail.com>
2020-08-25 13:55:05 -0600

    Close connection on application close

--- JChat/src/com/jchat/client/GUI.java
+++ JChat/src/com/jchat/client/GUI.java
@@ -3,6 +3,8 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +40,12 @@ public class GUI extends JFrame {
 					GUI frame = new GUI();
 
 					frame.setVisible(true);
+					frame.addWindowListener(new WindowAdapter() {
+						public void windowClosing(WindowEvent we) {
+							frame.chatClient.closeConnection();
+							System.exit(0);
+						}
+					});
 //					thread.join();
 				} catch (Exception e) {
 					e.printStackTrace();
